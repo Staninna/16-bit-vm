4e67c80e60c505a6f54fe312d23c8e5273738103
Staninna <26458805+Staninna@users.noreply.github.com>
2022-08-24 10:13:19 +0200

    Renamed memory_mapper to mm

--- src/main.rs
+++ src/main.rs
@@ -28,121 +28,121 @@ fn main() {
     let stack = Memory::new(0x00FF); // 255 bytes
 
     // Create a new memory-mapper fto map different memory regions
-    let mut memory_mapper = MemoryMapper::new();
+    let mut mm = MemoryMapper::new();
 
     // Map the memory to the memory-mapper ( Memory / Stack )
-    memory_mapper.map(memory, 0x0000, 0xFF00, true);
-    memory_mapper.map(stack, 0xFF00, 0xFFFF, true);
+    mm.map(memory, 0x0000, 0xFF00, true);
+    mm.map(stack, 0xFF00, 0xFFFF, true);
 
     // Load program to memory
-    hardcode(&mut memory_mapper);
+    hardcode(&mut mm);
 
     // Create virtual machine
-    let mut cpu = CPU::new(memory_mapper);
+    let mut cpu = CPU::new(mm);
 
     // Run the program
     cpu.run(DEBUG);
 }
 
-fn hardcode(memory_mapper: &mut MemoryMapper) {
+fn hardcode(mm: &mut MemoryMapper) {
     let mut program_address = 0;
 
-    memory_mapper.set_byte(MOV_LIT_REG, program_address + 0);
-    memory_mapper.set_byte(0x00, program_address + 1);
-    memory_mapper.set_byte(0x01, program_address + 2); // 0x0001
-    memory_mapper.set_byte(R1, program_address + 3);
+    mm.set_byte(MOV_LIT_REG, program_address + 0);
+    mm.set_byte(0x00, program_address + 1);
+    mm.set_byte(0x01, program_address + 2); // 0x0001
+    mm.set_byte(R1, program_address + 3);
 
-    memory_mapper.set_byte(ADD_REG_REG, program_address + 4);
-    memory_mapper.set_byte(R1, program_address + 5);
-    memory_mapper.set_byte(R2, program_address + 6);
+    mm.set_byte(ADD_REG_REG, program_address + 4);
+    mm.set_byte(R1, program_address + 5);
+    mm.set_byte(R2, program_address + 6);
 
-    memory_mapper.set_byte(JMP_NOT_EQ, program_address + 7);
-    memory_mapper.set_byte(0x00, program_address + 8);
-    memory_mapper.set_byte(0x14, program_address + 9); // 0x0064 / 100
-    memory_mapper.set_byte(0x00, program_address + 10);
-    memory_mapper.set_byte(0xFF, program_address + 11); // 0x00FF / 256
+    mm.set_byte(JMP_NOT_EQ, program_address + 7);
+    mm.set_byte(0x00, program_address + 8);
+    mm.set_byte(0x14, program_address + 9); // 0x0064 / 100
+    mm.set_byte(0x00, program_address + 10);
+    mm.set_byte(0xFF, program_address + 11); // 0x00FF / 256
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 12);
-    memory_mapper.set_byte(0xFF, program_address + 13);
-    memory_mapper.set_byte(0xFF, program_address + 14); // 0xFFFF
+    mm.set_byte(PSH_LIT, program_address + 12);
+    mm.set_byte(0xFF, program_address + 13);
+    mm.set_byte(0xFF, program_address + 14); // 0xFFFF
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 15);
-    memory_mapper.set_byte(0xEE, program_address + 16);
-    memory_mapper.set_byte(0xEE, program_address + 17); // 0xEEEE
+    mm.set_byte(PSH_LIT, program_address + 15);
+    mm.set_byte(0xEE, program_address + 16);
+    mm.set_byte(0xEE, program_address + 17); // 0xEEEE
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 18);
-    memory_mapper.set_byte(0xDD, program_address + 19);
-    memory_mapper.set_byte(0xDD, program_address + 20); // 0xDDDD
+    mm.set_byte(PSH_LIT, program_address + 18);
+    mm.set_byte(0xDD, program_address + 19);
+    mm.set_byte(0xDD, program_address + 20); // 0xDDDD
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 21);
-    memory_mapper.set_byte(0xCC, program_address + 22);
-    memory_mapper.set_byte(0xCC, program_address + 23); // 0xCCCC
+    mm.set_byte(PSH_LIT, program_address + 21);
+    mm.set_byte(0xCC, program_address + 22);
+    mm.set_byte(0xCC, program_address + 23); // 0xCCCC
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 24);
-    memory_mapper.set_byte(0xBB, program_address + 25);
-    memory_mapper.set_byte(0xBB, program_address + 26); // 0xBBBB
+    mm.set_byte(PSH_LIT, program_address + 24);
+    mm.set_byte(0xBB, program_address + 25);
+    mm.set_byte(0xBB, program_address + 26); // 0xBBBB
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 27);
-    memory_mapper.set_byte(0xAA, program_address + 28);
-    memory_mapper.set_byte(0xAA, program_address + 29); // 0xAAAA
+    mm.set_byte(PSH_LIT, program_address + 27);
+    mm.set_byte(0xAA, program_address + 28);
+    mm.set_byte(0xAA, program_address + 29); // 0xAAAA
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 30);
-    memory_mapper.set_byte(0x00, program_address + 31);
-    memory_mapper.set_byte(0x00, program_address + 32); // 0x0000
+    mm.set_byte(PSH_LIT, program_address + 30);
+    mm.set_byte(0x00, program_address + 31);
+    mm.set_byte(0x00, program_address + 32); // 0x0000
 
-    memory_mapper.set_byte(CAL_LIT, program_address + 33);
-    memory_mapper.set_byte(0xBA, program_address + 34);
-    memory_mapper.set_byte(0xAA, program_address + 35); // 0xBAAA
+    mm.set_byte(CAL_LIT, program_address + 33);
+    mm.set_byte(0xBA, program_address + 34);
+    mm.set_byte(0xAA, program_address + 35); // 0xBAAA
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 36);
-    memory_mapper.set_byte(0xAB, program_address + 37);
-    memory_mapper.set_byte(0xCD, program_address + 38); // 0xABCD
+    mm.set_byte(PSH_LIT, program_address + 36);
+    mm.set_byte(0xAB, program_address + 37);
+    mm.set_byte(0xCD, program_address + 38); // 0xABCD
 
-    memory_mapper.set_byte(HLT, program_address + 39);
+    mm.set_byte(HLT, program_address + 39);
 
     program_address = 0x00FF;
 
-    memory_mapper.set_byte(MOV_REG_REG, program_address + 0);
-    memory_mapper.set_byte(ACC, program_address + 1);
-    memory_mapper.set_byte(R2, program_address + 2);
+    mm.set_byte(MOV_REG_REG, program_address + 0);
+    mm.set_byte(ACC, program_address + 1);
+    mm.set_byte(R2, program_address + 2);
 
-    memory_mapper.set_byte(JMP_NOT_EQ, program_address + 3);
-    memory_mapper.set_byte(0xFF, program_address + 4);
-    memory_mapper.set_byte(0xFF, program_address + 5); // 0xFFFF
-    memory_mapper.set_byte(0x00, program_address + 6);
-    memory_mapper.set_byte(0x00, program_address + 7); // 0xFFFF
+    mm.set_byte(JMP_NOT_EQ, program_address + 3);
+    mm.set_byte(0xFF, program_address + 4);
+    mm.set_byte(0xFF, program_address + 5); // 0xFFFF
+    mm.set_byte(0x00, program_address + 6);
+    mm.set_byte(0x00, program_address + 7); // 0xFFFF
 
     program_address = 0xBAAA;
 
-    memory_mapper.set_byte(MOV_LIT_REG, program_address + 0);
-    memory_mapper.set_byte(0xAD, program_address + 1);
-    memory_mapper.set_byte(0xAD, program_address + 2); // 0xADAD
-    memory_mapper.set_byte(R3, program_address + 3);
+    mm.set_byte(MOV_LIT_REG, program_address + 0);
+    mm.set_byte(0xAD, program_address + 1);
+    mm.set_byte(0xAD, program_address + 2); // 0xADAD
+    mm.set_byte(R3, program_address + 3);
 
-    memory_mapper.set_byte(MOV_LIT_REG, program_address + 4);
-    memory_mapper.set_byte(0xAE, program_address + 5);
-    memory_mapper.set_byte(0xAE, program_address + 6); // 0xAEAE
-    memory_mapper.set_byte(R4, program_address + 7);
+    mm.set_byte(MOV_LIT_REG, program_address + 4);
+    mm.set_byte(0xAE, program_address + 5);
+    mm.set_byte(0xAE, program_address + 6); // 0xAEAE
+    mm.set_byte(R4, program_address + 7);
 
-    memory_mapper.set_byte(MOV_LIT_REG, program_address + 8);
-    memory_mapper.set_byte(0xAF, program_address + 9);
-    memory_mapper.set_byte(0xAF, program_address + 10); // 0xAFAF
-    memory_mapper.set_byte(R5, program_address + 11);
+    mm.set_byte(MOV_LIT_REG, program_address + 8);
+    mm.set_byte(0xAF, program_address + 9);
+    mm.set_byte(0xAF, program_address + 10); // 0xAFAF
+    mm.set_byte(R5, program_address + 11);
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 12);
-    memory_mapper.set_byte(0xAA, program_address + 13);
-    memory_mapper.set_byte(0xAA, program_address + 14); // 0xAAAA
+    mm.set_byte(PSH_LIT, program_address + 12);
+    mm.set_byte(0xAA, program_address + 13);
+    mm.set_byte(0xAA, program_address + 14); // 0xAAAA
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 15);
-    memory_mapper.set_byte(0xBB, program_address + 16);
-    memory_mapper.set_byte(0xBB, program_address + 17); // 0xBBBB
+    mm.set_byte(PSH_LIT, program_address + 15);
+    mm.set_byte(0xBB, program_address + 16);
+    mm.set_byte(0xBB, program_address + 17); // 0xBBBB
 
-    memory_mapper.set_byte(PSH_LIT, program_address + 18);
-    memory_mapper.set_byte(0xCC, program_address + 19);
-    memory_mapper.set_byte(0xCC, program_address + 20); // 0xCCCC
+    mm.set_byte(PSH_LIT, program_address + 18);
+    mm.set_byte(0xCC, program_address + 19);
+    mm.set_byte(0xCC, program_address + 20); // 0xCCCC
 
-    memory_mapper.set_byte(POP, program_address + 21);
-    memory_mapper.set_byte(R5, program_address + 22);
+    mm.set_byte(POP, program_address + 21);
+    mm.set_byte(R5, program_address + 22);
 
-    memory_mapper.set_byte(RET, program_address + 23);
+    mm.set_byte(RET, program_address + 23);
 }
